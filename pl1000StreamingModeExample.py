d3faa94a11773d8078c4da74dbf868b3cef9ac79
salinsalins <sanin_andrey@mail.ru>
2021-06-10 15:26:50 +0700

    commit

--- pl1000StreamingModeExample.py
+++ pl1000StreamingModeExample.py
@@ -18,7 +18,9 @@ from time import sleep, time
 class PicoLog1000:
     def __init__(self):
         self.handle = None
+        self.last_status = None
         self.opened = False
+        self.info = None
         self.channels = []
         self.sampling = None
         self.trigger = None
@@ -29,13 +31,15 @@ class PicoLog1000:
 
     def open(self):
         self.handle = ctypes.c_int16()
-        stat = pl.pl1000OpenUnit(ctypes.byref(self.handle))
-        assert_pico_ok(stat)
+        self.last_status = pl.pl1000OpenUnit(ctypes.byref(self.handle))
+        assert_pico_ok(self.last_status)
         self.opened = True
+        return True
 
     def assert_open(self):
         if not self.opened:
             raise ClosedDeviceError("PicoLog is not opened")
+        return True
         # handle = ctypes.c_int16()
         # progress = ctypes.c_int16()
         # complete = ctypes.c_int16()
@@ -43,16 +47,67 @@ class PicoLog1000:
         # assert_pico_ok(stat)
         # print('progress', handle, progress.value, complete.value)
 
-    def get_info(self):
+    def get_info(self, info=pl.PICO_HARDWARE_VERSION):
         self.assert_open()
-        self.info = (ctypes.c_int8 * 10)()
         length = ctypes.c_int16(10)
-        #stat = pl.pl1000GetUnitInfo(self.handle, None, length, ctypes.byref(length), INFO)
-        #assert_pico_ok(stat)
+        self.last_status = pl.pl1000GetUnitInfo(self.handle, None, length,
+                                                ctypes.byref(length), info)
+        self.info = (ctypes.c_int8 * length.value)()
+        self.last_status = pl.pl1000GetUnitInfo(self.handle, self.info, length.value,
+                                                ctypes.byref(length), info)
+        assert_pico_ok(self.last_status)
+        return self.info
+
+    def set_do(self, do_number, do_value):
+        self.assert_open()
+        self.last_status = pl.pl1000SetDo(self.handle, ctypes.c_int16(do_value), ctypes.c_int16(do_number))
+        assert_pico_ok(self.last_status)
+        return True
+
+    def set_pulse_width(self, period, cycle):
+        self.assert_open()
+        self.last_status = pl.pl1000SetPulseWidth(self.handle, ctypes.c_int16(period), ctypes.c_int8(cycle))
+        assert_pico_ok(self.last_status)
+        return True
+
+    def set_interval(self, dt_us, n_points, channels):
+        self.assert_open()
+        nc = len(channels)
+        cnls = (ctypes.c_int16 * nc)()
+        for i in range(len(cnls)):
+            cnls[i] = channels[i]
+        usForBlock = ctypes.c_uint32(dt_us)
+        noOfValues = ctypes.c_uint32(n)
+        self.last_status = pl.pl1000SetInterval(self.handle, ctypes.byref(usForBlock),
+                                                noOfValues, ctypes.byref(cnls), nc)
+        assert_pico_ok(self.last_status)
+        self.channels = channels
+        return True
+
+    dt_us = 1000000
+    n = 1000
+    cnls = (1, 2, 4, 7, 12, 15)
+    nc = len(cnls)
+    # set sampling interval
+    usForBlock = ctypes.c_uint32(dt_us)
+    noOfValues = ctypes.c_uint32(n)
+    channels = (ctypes.c_int16 * nc)()
+    for i in range(len(cnls)):
+        channels[i] = cnls[i]
+
+    txt = "setInterval"
+    print('---', txt)
+    t0 = time()
+    status[txt] = pl.pl1000SetInterval(chandle, ctypes.byref(usForBlock), noOfValues, ctypes.byref(channels), nc)
+    print('elapsed time', time() - t0, 's')
+    print(txt, 'status', status[txt])
+    assert_pico_ok(status[txt])
+    print('noOfValues', noOfValues.value, n)
+    print('usForBlock', usForBlock.value, dt_us)
 
     def set_channels(self, cnls=None):
         self.assert_open()
-        if cnls is None:
+        if cnls is None or len(cnls) <= 0:
             pass
         pass
 
@@ -70,14 +125,10 @@ print('elapsed time', time() - t0, 's')
 print(txt, 'status', status[txt])
 assert_pico_ok(status[txt])
 
-p1 = ctypes.c_int16(1000)
-p2 = ctypes.c_int8(50)
-p3 = ctypes.c_int16()
-#stat = pl.pl1000OpenUnitProgress(ctypes.byref(p1), ctypes.byref(p2), ctypes.byref(p3))
-#print('progress', handle, chandle, progress.value, complete.value)
-#stat = pl.pl1000OpenUnitAsync(ctypes.byref(handle))
-#stat = pl.pl1000PingUnit(chandle)
-stat = pl.pl1000SetPulseWidth(chandle, p1, p2)
+stat = pl.pl1000SetDo(chandle, ctypes.c_int16(1), ctypes.c_int16(1))
+assert_pico_ok(stat)
+
+stat = pl.pl1000SetPulseWidth(chandle, ctypes.c_int16(1000), ctypes.c_int8(50))
 assert_pico_ok(stat)
 
 # length = ctypes.c_int16(10)
@@ -88,7 +139,7 @@ assert_pico_ok(stat)
 # stat = pl.pl1000GetUnitInfo(chandle, ctypes.byref(info), length, ctypes.byref(length), pl.PICO_INFO['PICO_DRIVER_VERSION'])
 # assert_pico_ok(stat)
 
-dt_us = 8000
+dt_us = 1000000
 n = 1000
 cnls = (1, 2, 4, 7, 12, 15)
 nc = len(cnls)
@@ -132,7 +183,8 @@ print('--- wait ---')
 
 while not ready.value:
     # print('*')
-    assert_pico_ok(pl.pl1000Ready(chandle, ctypes.byref(ready)))
+    st = pl.pl1000Ready(chandle, ctypes.byref(ready))
+    assert_pico_ok(st)
 print('elapsed time', time() - t0, 's')
 print('noOfValues', noOfValues.value)
 print('usForBlock', usForBlock.value)
@@ -140,7 +192,7 @@ print('usForBlock', usForBlock.value)
 values = (ctypes.c_uint16 * (noOfValues.value * nc))()
 oveflow = ctypes.c_uint16()
 trigger = ctypes.c_uint32()
-print('len(values)', len(values), noOfValues.value)
+# print('len(values)', len(values), noOfValues.value)
 
 txt = "getValues"
 print('---', txt)
@@ -165,8 +217,8 @@ inputRange = 2500
 mVValues = adc2mVpl1000(values, inputRange, maxADC)
 
 # close PicoLog 1000 device
-status["closeUnit"] = pl.pl1000CloseUnit(chandle)
-assert_pico_ok(status["closeUnit"])
+# status["closeUnit"] = pl.pl1000CloseUnit(chandle)
+# assert_pico_ok(status["closeUnit"])
 
 # display status returns
 print(status)
