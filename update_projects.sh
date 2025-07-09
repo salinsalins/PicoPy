fc2a42a4ae48be02b9bc9cc060e54146bd3995a3
salinsalins <sanin_andrey@mail.ru>
2025-07-09 16:56:28 +0700

    Minor changes

---
+++ update_projects.sh
@@ -0,0 +1,45 @@
+cd PycharmProjects
+cd TangoUtils
+git fetch origin
+git rebase origin
+cd ..
+cd LoggerPlotterPy
+git fetch origin
+git rebase origin
+cd ..
+cd PicoPy
+git fetch origin
+git rebase origin
+cd ..
+cd 1_Mev_UI
+git fetch origin
+git rebase origin
+cd ..
+cd TDKLambda
+git fetch origin
+git rebase origin
+cd ..
+cd TangoShotDumperPy
+git fetch origin
+git rebase origin
+cd ..
+cd Tectronix
+git fetch origin
+git rebase origin
+cd ..
+cd IT6900
+git fetch origin
+git rebase origin
+cd ..
+cd LoggerDumperPy
+git fetch origin
+git rebase origin
+cd ..
+cd VasyaPy
+git fetch origin
+git rebase origin
+cd ..
+cd RunningLogger
+git fetch origin
+git rebase origin
+cd ..
